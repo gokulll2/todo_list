019a776f130ec89c4468602264137f0f73fa132f
Gokul <gokulsethi69@gmail.com>
2023-05-02 01:17:42 +0530

    main

---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Header()
+{
+    return(
+        <div className="head">
+           <h3>Add a new list here</h3> 
+        </div>
+    )
+}
---
+++ src/Components/Todo.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Header from "./Header"
+
+export default function Todo()
+{
+    return(
+        <div style={{"backgroundColor":"white","minHeight":"20vh"}}>
+            <Header/>
+        </div>
+    )
+}
